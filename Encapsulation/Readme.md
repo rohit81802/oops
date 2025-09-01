8bb6a848ba560ab0441d3932134bae15dfbf0991
Rohit <rohanbhoj2@gmail.com>
2025-09-01 09:30:15 +0530

    Encapsulation in C++

---
+++ Encapsulation/Readme.md
@@ -0,0 +1,5 @@
+# encapsulation 
+Encapsulation is wrapping up of data & member function in a Single unit class.
+
+# data Hiding
+Encapsulation is helpfull in data Hiding. by giving a class private : property
---
+++ Encapsulation/account.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+//encapsulation of data 
+class account{
+    public :
+    string name;
+    string id;
+    //data hiding
+    private :
+    double balance;
+    double Password;
+
+
+};
+int main(){
+
+    return 0;
+}
